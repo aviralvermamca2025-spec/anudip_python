2b036d485a940c4694eb951be250cd0ddd6df63f
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-20 09:44:10 +0530

    all program

---
+++ Selection_Statement_lab2/18_feb/SelfLearning/iteration/8.palindrome.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+temp = num
+rev = 0
+while temp > 0:
+    dig = temp % 10
+    rev = rev * 10 + dig
+    temp //= 10
+if num == rev:
+    print(num, "is a palindrome")
+else:    
+    print(num, "is not a palindrome")
