3687f73e4eb3fac2a9e37895ec38aab205fa2c7e
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-24 23:04:55 +0530

    Adding 7.Cal_Fahren_to_Cel.py file

---
+++ 100_Python_Question/7.Cal_Fahren_to_Cel.py
@@ -0,0 +1,13 @@
+#take input Fahrenheit
+Fahrenheit = float(input("Enter the fahrenheit temperature: "))
+
+# Convert to Celsius
+celsius = (Fahrenheit - 32) * 5/9
+
+# Print result
+print("Convertion of Fahrenheit to Celsius is:", celsius)
+
+#output
+#Enter the fahrenheit temperature:77
+#Convertion of Fahrenheit to Calcius is: 25.0
+
