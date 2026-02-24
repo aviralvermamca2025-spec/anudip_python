6c956befa71fb0fdbfa9ac6b720b94e66a1b9d2b
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-24 22:55:40 +0530

    Adding 3.Largest_three_number.py file

---
+++ 100_Python_Question/3.Largest_three_number.py
@@ -0,0 +1,20 @@
+#take three input a,b,c
+a = int(input("Enter the First number: "))
+b = int(input("Enter the Second number: "))
+c = int(input("Enter the Third number: "))
+
+#using Conditional Statements
+if(a > b and a > c):
+    print("a is Largest",a)
+
+elif(b > c):
+    print("b is Largest",b)
+
+else:
+    print("c is Largest",c)
+
+#output
+# a is Largest: 45
+
+
+
