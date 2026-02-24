d65354494cde24b10f862032a79cfbc61cedefa6
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-24 21:25:45 +0530

    1.Swap_two_num.py

---
+++ 100_Python_Question/1.Swap_two_num.py
@@ -0,0 +1,17 @@
+#take two input a and b
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+#Swaping of two numbers without using third variable
+a = a + b;
+b = a - b;
+a = a - b;
+
+print("After swaping: ")
+print("First number:",a)
+print("Second number:",b)
+
+#output
+#After swaping:
+#First number:45
+#Second number:34
+
