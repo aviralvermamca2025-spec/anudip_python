d50a7c56dab0abfd0e198402f0b658545502cc22
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-24 22:53:22 +0530

    Adding 2.Num_pos_neg.py file

---
+++ 100_Python_Question/2.Num_pos_neg.py
@@ -0,0 +1,19 @@
+# Enter your first number
+num = int(input("Enter your number: "))
+
+#use Conditional Statements
+if(num > 0):
+    print("Positive")
+
+elif(num < 0):
+    print("Negative")
+
+else:
+    print("Zero")
+
+print(num)
+
+#output
+#Enter your number:45
+# Positive
+# 45
