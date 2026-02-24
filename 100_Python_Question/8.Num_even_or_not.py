62eacd163fbb755de65c02788ea0dd20913a6162
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-24 23:06:20 +0530

    Adding 8.Num_even_or_not.py file

---
+++ 100_Python_Question/8.Num_even_or_not.py
@@ -0,0 +1,12 @@
+# Read input
+num = int(input("Enter the Number: "))
+
+# Check even or odd without %
+if (num // 2) * 2 == num:
+    print("Even")
+else:
+    print("Odd")
+
+#output
+#Enter the Number: 6
+#Even
