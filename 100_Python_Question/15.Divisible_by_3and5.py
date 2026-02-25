9e7f24b4cf8f27b5b456bb45ba26442b3724e456
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-25 15:46:20 +0530

    Adding 15.Divisible_by_3and5.py file

---
+++ 100_Python_Question/15.Divisible_by_3and5.py
@@ -0,0 +1,10 @@
+#
+num = int(input("Enter a number : "))
+if(num % 3 == 0 and num % 5 == 0):
+    print("Divisible by both 3 and 5")
+else:
+    print("Not Divisible by 3 and 5")
+
+#Output
+#Enter a number : 15
+#Divisible by both 3 and 5
