e88917071cfaa89546d7eadf1c0a8e48d7f39b09
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-25 15:50:15 +0530

    Adding 19.Number_range_or_not.py file

---
+++ 100_Python_Question/19.Number_range_or_not.py
@@ -0,0 +1,15 @@
+num = float(input("Enter a number : "))
+lower_bound = float(input("Enter the lower bound of the range : "))
+upper_bound = float(input("Enter the lower bound of the range : "))
+if(lower_bound <= num <= upper_bound):
+    print("The number lies within the range.")
+else:
+    print("The number does not lie within the range.")
+
+#Output
+#Enter a number : 21
+# Enter the lower bound of the range : 0
+# Enter the upper bound of the range :
+        
+
+
