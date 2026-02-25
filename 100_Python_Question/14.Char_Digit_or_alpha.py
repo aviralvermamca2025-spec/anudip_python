0f19b3d267188281fd9bf23987b9a0387c4660e7
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-25 15:45:23 +0530

    Adding 14.Char_Digit_or_alpha.py file

---
+++ 100_Python_Question/14.Char_Digit_or_alpha.py
@@ -0,0 +1,16 @@
+ch = input("Enter something: ")
+if(ch.isalpha()):
+    print("Alphabet")
+
+elif(ch.isdigit()):
+    print("Digit")
+
+elif(ch.isalnum()):
+    print("charactor is alphanumeric")
+
+else:
+    print("Special Charactor")
+
+#Output
+#Enter something
+#Digit
