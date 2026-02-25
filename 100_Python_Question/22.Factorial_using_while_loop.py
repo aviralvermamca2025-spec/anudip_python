ad0d08a5819cdb97aa6498f3d07b930b463610f5
aviralvermamca2025-spec <aviral.verma.mca.2025@miet.ac.in>
2026-02-25 15:55:28 +0530

    Adding 22.Factorial_using_while_loop.py file

---
+++ 100_Python_Question/22.Factorial_using_while_loop.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number : "))
+fact = 1
+i = 1
+while(num >= i):
+    fact = fact * i
+    i = i + 1
+
+print("factorial of", num,"is",fact)
+
+#Output
+#Enter a number : 5
+#factorial of 5 is 120
